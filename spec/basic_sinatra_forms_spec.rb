e6c264f773876d62972f2c3a2ef2d93c416a25d0
James Randolph <james.randolph2016@gmail.com>
2019-02-06 22:14:09 +0000

    Automatically backed up by Learn

--- spec/basic_sinatra_forms_spec.rb
+++ spec/basic_sinatra_forms_spec.rb
@@ -1,23 +1,6 @@
 describe App do
 
   describe 'GET /newteam' do
-    it 'sends a 200 status code' do
-      get '/newteam'
-      expect(last_response.status).to eq(200)
-    end
-
-    it 'renders basketball team form' do 
-      visit '/newteam'
-      expect(page).to have_selector("form")
-      expect(page).to have_field(:name)
-      expect(page).to have_field(:coach)
-      expect(page).to have_field(:pg)
-      expect(page).to have_field(:sg)
-      expect(page).to have_field(:sf)
-      expect(page).to have_field(:pf)
-      expect(page).to have_field(:c)
-    end
-  end
 
 
 
