9e74de045b9e2a33281431e4133a3e737bb130c2
James Randolph <james.randolph2016@gmail.com>
2019-02-06 22:13:48 +0000

    Automatically backed up by Learn

--- spec/basic_sinatra_forms_spec.rb
+++ spec/basic_sinatra_forms_spec.rb
@@ -28,59 +28,8 @@ describe App do
       expect(page).to have_text("Team Name: Bballers")
     end
 
-    it "displays the coach's name in the browser" do
-      visit '/newteam'
 
-      fill_in(:coach, :with => "Walter")
-      click_button "submit"
 
-      expect(page).to have_text("Coach: Walter")
-    end
-
-    it "displays the point guard's name in the browser" do
-      visit '/newteam'
-
-      fill_in(:pg, :with => "Jeff")
-      click_button "submit"
-
-      expect(page).to have_text("Point Guard: Jeff")
-    end
-
-    it "displays the shooting guard's name in the browser" do
-      visit '/newteam'
-
-      fill_in(:sg, :with => "Ralph")
-      click_button "submit"
-
-      expect(page).to have_text("Shooting Guard: Ralph")
-    end
-
-    it "displays the power forward's name in the browser" do
-      visit '/newteam'
-
-      fill_in(:pf, :with => "Danny")
-      click_button "submit"
-
-      expect(page).to have_text("Power Forward: Danny")
-    end
-
-    it "displays the shooting gaurd's name in the browser" do
-      visit '/newteam'
-
-      fill_in(:sg, :with => "Joe")
-      click_button "submit"
-
-      expect(page).to have_text("Shooting Guard: Joe")
-    end
-
-    it "displays the center's name in the browser" do
-      visit '/newteam'
-
-      fill_in(:c, :with => "Avi")
-      click_button "submit"
-
-      expect(page).to have_text("Center: Avi")
-    end
 
 
   end
